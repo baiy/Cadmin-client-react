f1cd40ef321d2cf74eb52674af33346ed552ad58
baiy <wo@baiy.org>
2019-11-29 16:01:11 +0800

    演示站点

---
+++ build/precache-manifest.c803f0c9307d1b177350e10cc5f47730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f139337937d70858c81fbf3e14928bdc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bebffeade6ecaf4362e9",
+    "url": "./static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "2c28de42ea384db1711f",
+    "url": "./static/css/main.ae2dfd78.chunk.css"
+  },
+  {
+    "revision": "bebffeade6ecaf4362e9",
+    "url": "./static/js/2.cfc2cb4e.chunk.js"
+  },
+  {
+    "revision": "2c28de42ea384db1711f",
+    "url": "./static/js/main.686dbca5.chunk.js"
+  },
+  {
+    "revision": "aef06153b1f795e20d43",
+    "url": "./static/js/runtime-main.463c4636.js"
+  }
+]);
